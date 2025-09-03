f42b86c4d82c406f67f5b3959418aeeab88a8f5c
enzostall <enzostall@alunos.utfpr.edu.br>
2025-09-03 11:03:34 -0300

    Create ex_5_lista2_rep.c

---
+++ ex_5_lista2_rep.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    srand(time(NULL));
+    int magico = rand() %500 +1;
+    int palpite;
+    int tentativas = 0;
+    
+    printf("JOGO DO ADVINHA \n");
+    printf("Sorteei um numero magico entre 1 e 500. Tente advinhar!\n\n");
+    
+    do {
+        printf("Digite seu palpite: ");
+        scanf("%d", &palpite);
+        tentativas++;
+        if (palpite < magico)
+        {
+            printf("O numero eh maior.\n");
+        }
+        else if (palpite > magico) {
+            printf("O numero eh menor.\n");
+        }
+    }
+    while (palpite != magico);
+    printf("\nPARABENS! voce acertou o numero %d em %d tentativas!\n", magico, tentativas);
+    
+    return 0;
+}
