b3cfdcf3f04230a4e2696affcf3d1e9bfa7b9dad
enzostall <enzostall@alunos.utfpr.edu.br>
2025-09-03 11:42:02 -0300

    Update Ex4_lista3_rep.c
    ARRUMAR CODIGO

--- Ex4_lista3_rep.c
+++ Ex4_lista3_rep.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+int main() {
+    int n, numero_og;
+    int digito_ref;
+    int digito_atual;
+    int sao_iguais = 1;
+    
+    printf("Digite um valor inteiro: \n");
+    scanf("%d", &n);
+    numero_og = n;
+    
+    if (n < 0) {
+        n= -n;
+    }
+    if (n < 10) {
+        printf("Sim, os digitos de %d sao todos iguais.\n", numero_og);
+        return 0;
+    }
+    digito_ref = n % 10;
+    
+    while (n > 0) {
+        digito_atual = n % 10;
+        if (digito_atual != digito_ref) {
+            sao_iguais = 0;
+            break;
+        }
+    }
+    n = n / 10;
+    if (sao_iguais) { 
+        if (sao_iguais == 1)
+        printf("Sim, os digitos de %d sao todos iguais entre si. \n", numero_og);
+    } else {
+        printf("Nao, os digitos de %d nao sao todos iguais entre si. \n", numero_og);
+    }
+    return 0;
+}
 
+
+ARRUMAR CODIGO
